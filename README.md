19b8f93669ce64c6cd7bf22e32a613edb2341e37
zhulj@aupup.com <c-zhulj@vanke.com>
2021-09-27 09:32:40 +0800

    qiankun debugger

---
+++ README.md
@@ -0,0 +1,8 @@
+# qiankun 源码阅读库
+
+- fork源官方库
+
+`examples/main原引入打包后的es文件，不方便调试`
+
+`新增examples/main-vue3+ts, 直接引用/src，方便调试`
+
---
+++ examples/main-vue3+ts/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <div class="mainapp">
+    <!-- 标题栏 -->
+    <header class="mainapp-header">
+      <h1>QianKun</h1>
+    </header>
+    <div class="mainapp-main">
+      <!-- 侧边栏 -->
+      <ul class="mainapp-sidemenu">
+        <li @click="push('/react16')">React16</li>
+        <li @click="push('/react15')">React15</li>
+        <li @click="push('/vue')">Vue</li>
+        <li @click="push('/vue3')">Vue3</li>
+        <li @click="push('/angular9')">Angular9</li>
+        <li @click="push('/purehtml')">Purehtml</li>
+      </ul>
+      <!-- 子应用  -->
+      <main id="subapp-viewport"></main>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  methods: {
+    push(subapp: string) {
+      history.pushState(null, subapp, subapp)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.mainapp-header {
+  text-align: left;
+}
+#subapp-viewport {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
